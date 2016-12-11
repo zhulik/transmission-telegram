edcbd6ac5a8a61c4516cb77d5557d6d262cc5e92
Gleb Sinyavsky <zhulik.gleb@gmail.com>
2016-12-11 17:41:16 +0300

    notification settings

--- settings/settings.go
+++ settings/settings.go
@@ -2,49 +2,21 @@ package settings
 
 import (
 	"github.com/boltdb/bolt"
+	// "log"
 	"strconv"
+)
+
+const (
+	users_bucket  = "transmission-telegram-users"
+	notify_bucket = "transmission-telegram-notify"
 )
 
 type Settings interface {
 	SetUserID(string, int64) error
 	GetUserID(string) (int64, error)
+	SetUserNotification(string, bool) error
+	GetUserNotification(string) (bool, error)
 	Close()
-}
-
-const (
-	users_bucket = "transmission-telegram-users"
-)
-
-type settings struct {
-	db *bolt.DB
-}
-
-func (s *settings) SetUserID(username string, chatID int64) error {
-	err := s.db.Update(func(tx *bolt.Tx) error {
-		b, err := tx.CreateBucketIfNotExists([]byte(users_bucket))
-		if err != nil {
-			return err
-		}
-		return b.Put([]byte(username), []byte(strconv.FormatInt(chatID, 10)))
-	})
-	s.db.Sync()
-	return err
-}
-
-func (s *settings) GetUserID(username string) (int64, error) {
-	var result int64
-	err := s.db.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte(users_bucket))
-		v := b.Get([]byte(username))
-		var err error
-		result, err = strconv.ParseInt(string(v), 10, 64)
-		return err
-	})
-	return result, err
-}
-
-func (s *settings) Close() {
-	s.db.Close()
 }
 
 func GetSettings(path string) (Settings, error) {
@@ -53,4 +25,66 @@ func GetSettings(path string) (Settings, error) {
 		return nil, err
 	}
 	return &settings{db: db}, nil
+}
+
+type settings struct {
+	db *bolt.DB
+}
+
+func (s *settings) SetUserID(username string, chatID int64) error {
+	err := s.set(users_bucket, username, strconv.FormatInt(chatID, 10))
+	return err
+}
+
+func (s *settings) GetUserID(username string) (int64, error) {
+	val, err := s.get(users_bucket, username)
+	if err != nil {
+		return 0, err
+	}
+	return strconv.ParseInt(string(val), 10, 64)
+}
+
+func (s *settings) SetUserNotification(username string, notify bool) error {
+	return s.set(notify_bucket, username, strconv.FormatBool(notify))
+}
+
+func (s *settings) GetUserNotification(username string) (bool, error) {
+	v, err := s.get(notify_bucket, username)
+	if err != nil {
+		return false, err
+	}
+	b, err := strconv.ParseBool(v)
+	if err != nil {
+		return false, nil
+	}
+	return b, nil
+}
+
+func (s *settings) set(bucket string, key string, value string) error {
+	err := s.db.Update(func(tx *bolt.Tx) error {
+		b, err := tx.CreateBucketIfNotExists([]byte(bucket))
+		if err != nil {
+			return err
+		}
+		return b.Put([]byte(key), []byte(value))
+	})
+	s.db.Sync()
+	return err
+}
+
+func (s *settings) get(bucket string, key string) (string, error) {
+	var result string
+	err := s.db.Update(func(tx *bolt.Tx) error {
+		b, err := tx.CreateBucketIfNotExists([]byte(bucket))
+		if err != nil {
+			return err
+		}
+		result = string(b.Get([]byte(key)))
+		return nil
+	})
+	return result, err
+}
+
+func (s *settings) Close() {
+	s.db.Close()
 }
--- settings/settings_test.go
+++ settings/settings_test.go
@@ -2,27 +2,89 @@ package settings_test
 
 import (
 	"github.com/zhulik/transmission-telegram/settings"
+	"os"
 	"testing"
 )
 
-func TestSettings(t *testing.T) {
-	settings, err := settings.GetSettings("/tmp/settings.db")
+const (
+	path = "/tmp/settings.db"
+)
+
+func TestSetGetUserID(t *testing.T) {
+	os.Remove(path)
+	settings, err := settings.GetSettings(path)
 
 	if err != nil {
-		t.Fail()
+		t.Fatal(err)
 	}
 
 	err = settings.SetUserID("testuser", 100500)
 	if err != nil {
-		t.Fail()
+		t.Fatal(err)
 	}
 
 	id, err := settings.GetUserID("testuser")
 	if err != nil {
-		t.Fail()
+		t.Fatal(err)
 	}
 	if id != 100500 {
+		t.Fatal("Wrong id returned")
+	}
+	settings.Close()
+}
+
+func TestGetUserID(t *testing.T) {
+	os.Remove(path)
+	settings, err := settings.GetSettings(path)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = settings.GetUserID("testuser")
+	if err == nil {
+		t.Fatal(err)
+	}
+	settings.Close()
+}
+
+func TestSetGetNotification(t *testing.T) {
+	os.Remove(path)
+	settings, err := settings.GetSettings(path)
+
+	if err != nil {
 		t.Fail()
+	}
+
+	err = settings.SetUserNotification("testuser", true)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	n, err := settings.GetUserNotification("testuser")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !n {
+		t.Fatal("Wrong value returned")
+	}
+	settings.Close()
+}
+
+func TestGetNotification(t *testing.T) {
+	os.Remove(path)
+	settings, err := settings.GetSettings(path)
+
+	if err != nil {
+		t.Fail()
+	}
+
+	n, err := settings.GetUserNotification("testuser")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if n {
+		t.Fatal("Wrong value returned")
 	}
 	settings.Close()
 }
